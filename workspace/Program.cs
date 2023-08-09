7b4611aad5b063f83a69f1d575e0e5fa6c397be4
Trent Millar <trent.millar@gmail.com>
2023-08-09 16:41:36 -0600

    committing file, Program.cs

---
+++ workspace/Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Program // This class encapsulates the entire console application
+{
+    public static void Main(string[] args) // This method is the main entry point of the application
+    {
+        System.Console.WriteLine("Hello World"); // This line of code prints "Hello World" to the console.
+    }
+}
